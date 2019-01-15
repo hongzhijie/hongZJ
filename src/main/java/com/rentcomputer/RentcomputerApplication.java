100a5ef339e9050f779bb1554a5e04a5f592f921
hongzhijie <hzj_777@163.com>
2019-01-15 09:58:49 +0800

    添加线程池配置

--- src/main/java/com/rentcomputer/RentcomputerApplication.java
+++ src/main/java/com/rentcomputer/RentcomputerApplication.java
@@ -2,6 +2,7 @@ package com.rentcomputer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /*
@@ -16,6 +17,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 //启用线程池
 @EnableAsync
+//启用缓存
+@EnableCaching
 public class RentcomputerApplication {
 
 	public static void main(String[] args) {
